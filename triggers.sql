fd7ec1f2a2828291bcc9d0326b2239d67d6f103a
rmarvin1 <rmarvin1@harveyv.binghamton.edu>
2021-11-12 00:58:36 -0500

    doublechecked upto 6

--- triggers.sql
+++ triggers.sql
@@ -1,20 +1,24 @@
-create or replace trigger add_to_students
+	create or replace trigger add_to_students
 after insert on students
 begin
 	dbms_output.put_line('A new students has been added');
 end add_to_students;
 /
 create or replace trigger delete_to_enrollments
-after delete on enrollments
+before delete on enrollments
+for each row
 begin
-	dbms_output.put_line('A deletion in enrollments has occurred');
+	begin
+		update classes set class_size = class_size - 1
+		where classid = :OLD.classid;
+	end;
 end delete_to_enrollments;
 /
 create or replace trigger delete_to_students
 before delete on students
 for each row
 begin
-	delete from enrollments where sid = :old.sid;
+	delete from enrollments where sid = :OLD.sid;
 end delete_to_students;
 /
 create or replace trigger add_to_enrollments
