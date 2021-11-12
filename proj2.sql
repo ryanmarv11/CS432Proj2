d818592cf95795a63c1026d6a0ecc747d6dba31d
ryanmarv11 <30666230+ryanmarv11@users.noreply.github.com>
2021-11-12 00:59:44 -0500

    Update proj2.sql

--- proj2.sql
+++ proj2.sql
@@ -151,65 +151,59 @@ CREATE OR REPLACE PACKAGE BODY proj2 AS
 				END LOOP;
 			END IF;
 		END print_enr;
-		procedure q7_enroll(sid_in in students.sid%type, cid_in in classes.classid%type) AS
-			cursor c1 is SELECT s.sid from students s where s.sid = sid_in;
-			c1_rec c1%rowtype;
-            		cursor c2 is SELECT c.classid from classes c where c.classid = cid_in;
-            		c2_rec c2%rowtype;
-            		cursor c3 is SELECT c.limit, c.class_size from classes c where c.classid = cid_in;
-           		c3_rec c3%rowtype;
-            		cursor c4 is SELECT * FROM (SELECT e.classid from enrollments e where e.sid = sid_in) t1 WHERE t1.classid = cid_in;
-            		c4_rec c4%rowtype;
-            		-- alreadyInClass char(5);
-            		cnt number;
-            		cursor c5 is
-            		SELECT t5.sid FROM
-            		(SELECT e.sid FROM enrollments e,
-            		(SELECT classid FROM classes,
-            		(SELECT pre.pre_dept_code, pre.pre_course_no FROM prerequisites pre,
-            		(SELECT c.dept_code, c.course_no FROM classes c WHERE c.classid = cid_in) t2
-            		WHERE pre.dept_code=t2.dept_code AND pre.course_no=t2.course_no) t3
-            		WHERE classes.dept_code=t3.pre_dept_code AND classes.course_no=t3.pre_course_no) t4
-            		WHERE e.classid=t4.classid AND (e.lgrade='A' OR e.lgrade='B' OR e.lgrade='C')) t5
-            		WHERE sid_in=t5.sid;
-            		c5_rec c5%rowtype;
+procedure q7_enroll(sid_in in students.sid%type, cid_in in classes.classid%type) AS
+                        cursor c1 is SELECT s.sid from students s where s.sid = sid_in;
+                        c1_rec c1%rowtype;
+                        cursor c2 is SELECT c.classid from classes c where c.classid = cid_in;
+                        c2_rec c2%rowtype;
+                        cursor c3 is SELECT c.limit, c.class_size from classes c where c.classid = cid_in;
+                        c3_rec c3%rowtype;
+                        cursor c4 is SELECT * FROM (SELECT e.classid from enrollments e where e.sid = sid_in) t1 WHERE t1.classid = cid_in;
+                        c4_rec c4%rowtype;
+                        -- alreadyInClass char(5);
+                        cnt number;
+                        cursor c5 is
+                        SELECT t5.sid FROM
+                        (SELECT e.sid FROM enrollments e,
+                        (SELECT classid FROM classes,
+                        (SELECT pre.pre_dept_code, pre.pre_course_no FROM prerequisites pre,
+                        (SELECT c.dept_code, c.course_no FROM classes c WHERE c.classid = cid_in) t2
+                        WHERE pre.dept_code=t2.dept_code AND pre.course_no=t2.course_no) t3
+                        WHERE classes.dept_code=t3.pre_dept_code AND classes.course_no=t3.pre_course_no) t4
+                        WHERE e.classid=t4.classid AND (e.lgrade='A' OR e.lgrade='B' OR e.lgrade='C')) t5
+                        WHERE sid_in=t5.sid;
+                     	c5_rec c5%rowtype;
+ 		BEGIN
+                     	OPEN c1;
+                        FETCH c1 into c1_rec;
+                        OPEN c2;
+                        FETCH c2 into c2_rec;
+                        OPEN c3;
+                        FETCH c3 into c3_rec;                        
+                        OPEN c4;
+                        FETCH c4 into c4_rec;                        
+                        OPEN c5;
+                        FETCH c5 into c5_rec;                        
+                        IF c1%notfound = true THEN
+                                dbms_output.put_line('sid not found');
+                        ELSIF c2%notfound = true THEN
+                                dbms_output.put_line('invalid classid');
+                        ELSIF c3_rec.limit = c3_rec.class_size THEN
+                                dbms_output.put_line('class full');
+                        -- SELECT e.classid into already from enrollments e where e.sid = sid_in;
+                        ELSIF c4%found = true THEN
+                                dbms_output.put_line('already in this class');
+                        	SELECT count(e.classid) into cnt from enrollments e where e.sid = sid_in;
+                        -- dbms_output.put_line(cnt);
+                        ELSIF cnt > 3 THEN
+                                dbms_output.put_line('overloaded');
+                        ELSIF c5%notfound = true THEN
+                                -- dbms_output.put_line(c5_rec.classid);
+                                dbms_output.put_line('Prerequisite courses havent been completed.');
+                        ELSE
+                        	insert into enrollments values(sid_in, cid_in, 'I');
+                END q7_enroll;
 
-        	BEGIN
-            		OPEN c1;
-            		FETCH c1 into c1_rec;
-            		IF c1%notfound = true THEN
-                		dbms_output.put_line('sid not found');
-            		END IF;
-            		OPEN c2;
-            		FETCH c2 into c2_rec;
-            		IF c2%notfound = true THEN
-                		dbms_output.put_line('invalid classid');
-            		END IF; 
-            		OPEN c3;
-            		FETCH c3 into c3_rec;
-            		IF c3_rec.limit = c3_rec.class_size THEN
-                		dbms_output.put_line('class full');
-            		END IF;
-
-            		-- SELECT e.classid into already from enrollments e where e.sid = sid_in;
-            		OPEN c4;
-            		FETCH c4 into c4_rec;
-            		IF c4%found = true THEN
-                		dbms_output.put_line('already in this class');
-            		END IF; 
-		        SELECT count(e.classid) into cnt from enrollments e where e.sid = sid_in;
-            		-- dbms_output.put_line(cnt);
-            		IF cnt > 3 THEN
-                		dbms_output.put_line('overloaded');
-            		END IF;
-
-            		OPEN c5;
-            		FETCH c5 into c5_rec;
-            		IF c5%notfound = true THEN
-                		-- dbms_output.put_line(c5_rec.classid);
-                		dbms_output.put_line('Prerequisite courses havent been completed.');
-            		END IF;
-		END q7_enroll;
 		procedure drop_student(si in students.sid%type, cid in classes.classid%type) AS
 			cursor c1 is select * from students where si = students.sid;
 			c1_rec c1%rowtype;
